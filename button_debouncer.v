cad1c825e5ce99d9c05a80df8f8189fb7b432176
PaladinBIGDan2077 <daniellomis@yahoo.com>
2025-09-27 17:05:10 -0400

    reverted button debouncer

--- button_debouncer.v
+++ button_debouncer.v
@@ -69,7 +69,7 @@ module button_debouncer (clk, rst_n, btn_n_in, pulse_out);
         end
     end
 
-    always @(current_state) begin
+    always @(*) begin
         // Default assignments
         next_state = current_state;
         pulse_trigger = 1'b0;
