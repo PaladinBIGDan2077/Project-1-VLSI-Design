f8271751489199c42119524fb2f50049e26d7415
PaladinBIGDan2077 <daniellomis@yahoo.com>
2025-09-28 16:03:29 -0400

    second fix

--- floor_logic.sv
+++ floor_logic.sv
@@ -282,7 +282,7 @@ always @(*) begin
                 memory_pointer = memory_pointer + 1;
             end
         end
-        if (|call_button_lights || |panel_button_lights && (elevator_floor_selector == current_floor_state)) begin
+        if (|call_button_lights || |panel_button_lights && (elevator_floor_selector == current_floor_state) && !elevator_moving) begin
                 memory_pointer = memory_pointer_temporary;
         end
         if (!elevator_moving && (|call_button_lights || |panel_button_lights)) begin
